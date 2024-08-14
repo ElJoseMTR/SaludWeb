021d599e99ad097bfb7de0c8171c13015cd3ca7d
ElJoseMTR <167329174+ElJoseMTR@users.noreply.github.com>
2024-08-13 21:24:06 -0500

    Update verific.js

--- jss/verific.js
+++ jss/verific.js
@@ -1,7 +1,51 @@
-if (localStorage.getItem('user') && localStorage.getItem('inicio')){
-    
-}else{
-    window.location.href = "../html/nopermision.html";
-    console.log("NO PERMISION")
+document.addEventListener('DOMContentLoaded',async function() {
+    const token = localStorage.getItem('token');
 
-}
+    if (token) {
+        try {
+            
+            const decoded = jwt_decode(token);
+            console.log('Token decodificado:', decoded);
+
+         
+            const user = decoded.user;
+            const password = decoded.password;
+            const exp = decoded.exp;
+
+           
+            const ahora = Math.floor(Date.now() / 1000);
+            if (ahora >= exp) {
+                console.log('el token se fue');
+                const respuesta = confirm("¿Quieres extender la seccion?");
+                if (respuesta) {
+
+                    const response = await fetch(`https://api-base-de-datos.onrender.com/login?user=${user}&password=${password}`, {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                    });
+                
+                    const data = await response.json();
+                    if (data.informacion === "Inicio de sesión exitoso") {
+                        localStorage.setItem('token', data.token);
+                    }else{
+                        window.location.href = '../../index.html';
+                    }
+                }else{
+                    window.location.href = '../../index.html';
+                    localStorage.removeItem('token');
+                    localStorage.removeItem('user');
+                }
+            } else {
+                console.log('usuae', user);
+                console.log('contraseña', password);
+            }
+        } catch (error) {
+            console.error('errors', error);
+        }
+    } else {
+        
+        window.location.href = '../../index.html';
+    }
+});
