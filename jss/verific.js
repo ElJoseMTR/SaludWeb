fe2616b72ecbb0badeabad12496bdda615c1022f
ElJoseMTR <167329174+ElJoseMTR@users.noreply.github.com>
2024-08-17 08:49:32 -0500

    Update verific.js

--- jss/verific.js
+++ jss/verific.js
@@ -29,32 +29,54 @@ document.addEventListener('DOMContentLoaded', async function () {
                         if (data.informacion === "Inicio de sesión exitoso") {
                             localStorage.setItem('token', data.token);
                         } else {
-                            const response = await fetch(`https://api-base-de-datos.onrender.com/loginAdmin?user=${user}&password=${password}`, {
-                                method: 'GET',
-                                headers: {
-                                    'Content-Type': 'application/json',
-                                },
-                            });
+                            
+                            verificarTokenAdmin();
+                        }
+                    } else {
+                        window.location.href = '../../index.html';
+                        localStorage.removeItem('token');
+                        localStorage.removeItem('user');
+                    }
+                } else {
 
-                            const data = await response.json();
+                }
+            } catch (error) {
+                console.error('errors', error);
+            }
+        } else {
+
+            window.location.href = '../../index.html';
+        }
+    }
+    async function verificarTokenAdmin() {
+        const token = localStorage.getItem('token');
+        if (token) {
+            try {
+                const decoded = jwt_decode(token);
+                const user = decoded.user;
+                const password = decoded.password;
+                const exp = decoded.exp;
+                const ahora = Math.floor(Date.now() / 1000);
+                if (ahora >= exp) {
+
+                    const respuesta = confirm("¿Quieres extender la seccion?");
+                    if (respuesta) {
+                        const response = await fetch(`https://api-base-de-datos.onrender.com/loginAdmin?user=${user}&password=${password}`, {
+                            method: 'GET',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                        });
+
+                        const data = await response.json();
+                        if (data.informacion === "Inicio de sesión exitoso") {
+                            localStorage.setItem('token', data.token);
+                        } else {
+                            
                             if (data.informacion === "Inicio de sesión exitoso") {
                                 localStorage.setItem('token', data.token);
                             } else {
-                                const response = await fetch(`https://api-base-de-datos.onrender.com/loginMedico?user=${user}&password=${password}`, {
-                                    method: 'GET',
-                                    headers: {
-                                        'Content-Type': 'application/json',
-                                    },
-                                });
-
-                                const data = await response.json();
-                                if (data.informacion === "Inicio de sesión exitoso") {
-                                    localStorage.setItem('token', data.token);
-                                } else {
-                                    window.location.href = '../../index.html';
-                                    localStorage.removeItem('token');
-                                    localStorage.removeItem('user');
-                                }
+                               verificarTokenMedico();
                             }
                         }
                     } else {
@@ -73,6 +95,56 @@ document.addEventListener('DOMContentLoaded', async function () {
             window.location.href = '../../index.html';
         }
     }
-    setInterval(verificarToken, 5000);
+    async function verificarTokenMedico() {
+        const token = localStorage.getItem('token');
+        if (token) {
+            try {
+                const decoded = jwt_decode(token);
+                const user = decoded.user;
+                const password = decoded.password;
+                const exp = decoded.exp;
+                const ahora = Math.floor(Date.now() / 1000);
+                if (ahora >= exp) {
+
+                    const respuesta = confirm("¿Quieres extender la seccion?");
+                    if (respuesta) {
+                        const response = await fetch(`https://api-base-de-datos.onrender.com/loginMedico?user=${user}&password=${password}`, {
+                            method: 'GET',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                        });
+
+                        const data = await response.json();
+                        if (data.informacion === "Inicio de sesión exitoso") {
+                            localStorage.setItem('token', data.token);
+                        } else {
+                            
+                            if (data.informacion === "Inicio de sesión exitoso") {
+                                localStorage.setItem('token', data.token);
+                            } else {
+                                window.location.href = '../../index.html';
+                                localStorage.removeItem('token');
+                                localStorage.removeItem('user');
+                            }
+                        }
+                    } else {
+                        window.location.href = '../../index.html';
+                        localStorage.removeItem('token');
+                        localStorage.removeItem('user');
+                    }
+                } else {
+
+                }
+            } catch (error) {
+                console.error('errors', error);
+            }
+        } else {
+
+            window.location.href = '../../index.html';
+        }
+    }
+    setInterval(verificarToken, 10000);
 });
+
 
